fbd76b2eec87a8a5b663461451870bc5869a8fd5
ashrakrahman <ashrakrahman@gmail.com>
2023-02-22 21:12:49 -0500

    Update load env variable for local dev

--- fast-backend/settings.py
+++ fast-backend/settings.py
@@ -1,8 +1,13 @@
 import os
 import motor.motor_asyncio
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
 
 # ----------------- Database variables (MongoDB) --------------------------s
 client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("DB_URL"))
-db = client.homeSiteDB
+
+print(os.getenv("DB_URL"))
+db = client.testDB1
 
 print("DB Client info ::", db)
